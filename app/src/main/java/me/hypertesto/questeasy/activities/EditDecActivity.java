9f3b89a2e67958b517a5fa967709b7d575f53837
gianLuke <sanar@live.it>
2016-04-29 11:38:35 +0200

    Implemented EditDecActivity and auxiliary data structures

--- app/src/main/java/me/hypertesto/questeasy/activities/EditDecActivity.java
+++ app/src/main/java/me/hypertesto/questeasy/activities/EditDecActivity.java
@@ -7,16 +7,41 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import me.hypertesto.questeasy.R;
+import me.hypertesto.questeasy.model.adapters.CardListAdapter;
+import me.hypertesto.questeasy.model.listitems.CardListItem;
 
 public class EditDecActivity extends AppCompatActivity {
 
     private FloatingActionButton gotoSelectCategory;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_dec);
+
+        ArrayList<CardListItem> items = new ArrayList<>();
+        items.add(new CardListItem("singolo","Pippo","Fuffa",true,null,null));
+        items.add(new CardListItem("famiglia", "Famiglia Rossi", "3 persone", false, null, null));
+        items.add(new CardListItem("gruppo", "Scolaresca", "25 persone", true, null, null));
+        items.add(new CardListItem("singolo","Pippo","Fuffa",true,null,null));
+        items.add(new CardListItem("famiglia","Famiglia Rossi","3 persone",false,null,null));
+        items.add(new CardListItem("gruppo","Scolaresca","25 persone",true,null,null));
+        items.add(new CardListItem("singolo","Pippo","Fuffa",true,null,null));
+        items.add(new CardListItem("famiglia","Famiglia Rossi","3 persone",false,null,null));
+        items.add(new CardListItem("gruppo","Scolaresca","25 persone",true,null,null));
+        items.add(new CardListItem("singolo","Pippo","Fuffa",true,null,null));
+        items.add(new CardListItem("famiglia","Famiglia Rossi","3 persone",false,null,null));
+        items.add(new CardListItem("gruppo","Scolaresca","25 persone",true,null,null));
+
+
+        CardListAdapter adapter = new CardListAdapter(this,R.layout.card_list_item,items);
+        listView = (ListView)findViewById(R.id.cardlistView);
+        listView.setAdapter(adapter);
 
         gotoSelectCategory = (FloatingActionButton)findViewById(R.id.categoryGuestGo);
         gotoSelectCategory.setOnClickListener(new View.OnClickListener() {
