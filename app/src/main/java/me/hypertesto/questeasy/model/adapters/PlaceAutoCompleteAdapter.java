f7e814f7b39d7bd8015fa249781bf2e24c329f57
Enrico Testori <hypertesto@gmail.com>
2016-05-09 15:19:52 +0200

    First working version of API request

--- app/src/main/java/me/hypertesto/questeasy/model/adapters/PlaceAutoCompleteAdapter.java
+++ app/src/main/java/me/hypertesto/questeasy/model/adapters/PlaceAutoCompleteAdapter.java
@@ -1,5 +1,7 @@
 package me.hypertesto.questeasy.model.adapters;
 
+import android.app.DownloadManager;
+import android.app.admin.SystemUpdatePolicy;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +11,23 @@ import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import me.hypertesto.questeasy.R;
 
@@ -63,11 +79,11 @@ public class PlaceAutoCompleteAdapter extends BaseAdapter implements Filterable 
 			protected FilterResults performFiltering(CharSequence constraint) {
 				Filter.FilterResults filterResults = new FilterResults();
 				if (constraint != null) {
-					List<String> books = findPlace(mContext, constraint.toString());
+					List<String> places = findPlace(mContext, constraint.toString());
 
 					// Assign the data to the FilterResults
-					filterResults.values = books;
-					filterResults.count = books.size();
+					filterResults.values = places;
+					filterResults.count = places.size();
 				}
 				return filterResults;
 			}
@@ -88,12 +104,41 @@ public class PlaceAutoCompleteAdapter extends BaseAdapter implements Filterable 
 	/**
 	 * Returns a search result for the given book title.
 	 */
-	private List<String> findPlace(Context context, String bookTitle) {
-		// GoogleBooksProtocol is a wrapper for the Google Books API
-		//GoogleBooksProtocol protocol = new GoogleBooksProtocol(context, MAX_RESULTS);
-		//return protocol.findBooks(bookTitle);
-		List<String> test = Arrays.asList("Buenos Aires", "Córdoba", "La Plata");
-		return test;
+	private List<String> findPlace(Context context, String place) {
+
+		RequestQueue queue = Volley.newRequestQueue(context);
+		String url ="https://questura.hypertesto.me/api/v1/comuni/" + place;
+		List<String> filteredPlaces = new ArrayList<>();
+		RequestFuture<JSONObject> future = RequestFuture.newFuture();
+		JsonObjectRequest request = new JsonObjectRequest(url, null, future, future);
+
+		System.out.println("[DEBUG] Sono qui");
+		queue.add(request);
+
+		try {
+			JSONObject response = future.get(); // this will block (forever)
+			JSONArray jArray = response.getJSONArray("comuni");
+			System.out.println(jArray);
+			for (int i=0; i < jArray.length(); i++) {
+				try {
+					JSONObject oneObject = jArray.getJSONObject(i);
+					// Pulling items from the array
+					filteredPlaces.add(oneObject.getString("nome"));
+					System.out.println("[DEBUG] " + oneObject.getString("nome"));
+
+				} catch (JSONException e) {
+					// Oops
+				}
+			}
+		} catch (InterruptedException e) {
+			// exception handling
+		} catch (ExecutionException e) {
+			// exception handling
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		return filteredPlaces;
 	}
 }
 
