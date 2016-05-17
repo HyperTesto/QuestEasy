8d3babc621a3074208154f5e59f1b0d0dde1e3e3
Enrico Testori <hypertesto@gmail.com>
2016-05-17 21:26:11 +0200

    Add missing TextBox

--- app/src/main/java/me/hypertesto/questeasy/model/adapters/GroupListAdapter.java
+++ app/src/main/java/me/hypertesto/questeasy/model/adapters/GroupListAdapter.java
@@ -51,8 +51,10 @@ public class GroupListAdapter extends ArrayAdapter<Guest>{
 
 		TextView txtName = (TextView) view.findViewById(R.id.guestNameAndSurname);
 		txtName.setText(String.format("%s %s", item.getName(), item.getSurname()));
+		TextView placeOfBirth = (TextView) view.findViewById(R.id.guestBirthPlace);
+		placeOfBirth.setText(item.getStatoDiNascita()); //FIXME: use new Place class
 
-				ImageView typeImg = (ImageView) view.findViewById(R.id.guestTypeImg);
+		ImageView typeImg = (ImageView) view.findViewById(R.id.guestTypeImg);
 
 		ColorGenerator generator = ColorGenerator.MATERIAL;
 		int color = generator.getRandomColor();
