c89cc049eb5ac1be602437961c82703b8b2a83ba
simoneclr <simone.clclr@gmail.com>
2016-05-19 16:20:28 +0200

    Clean populate

--- app/src/main/java/me/hypertesto/questeasy/activities/HomeActivity.java
+++ app/src/main/java/me/hypertesto/questeasy/activities/HomeActivity.java
@@ -102,8 +102,8 @@ public class HomeActivity extends AppCompatActivity {
 
 		FSDeclarationDao fsd = new FSDeclarationDao(this.getApplicationContext());
 
-		//fsd.clear();
-		//fsd.populate();
+		fsd.clear();
+		fsd.populate();
 
 		fsd.open();
 
