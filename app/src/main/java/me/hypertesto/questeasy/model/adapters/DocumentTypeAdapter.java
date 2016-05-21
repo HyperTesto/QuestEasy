b39e1dc114386754af4f19b744a7de132bc1c190
Enrico Testori <hypertesto@gmail.com>
2016-05-21 10:17:33 +0200

    Add DocumentTypeAdapter stub

---
+++ app/src/main/java/me/hypertesto/questeasy/model/adapters/DocumentTypeAdapter.java
@@ -0,0 +1,39 @@
+package me.hypertesto.questeasy.model.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+/**
+ * Created by hypertesto on 21/05/16.
+ */
+public class DocumentTypeAdapter extends BaseAdapter implements Filterable {
+
+	@Override
+	public int getCount() {
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		return null;
+	}
+
+	@Override
+	public Filter getFilter() {
+		return null;
+	}
+
+}
