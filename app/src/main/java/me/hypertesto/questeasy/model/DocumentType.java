f4d07099cc38eb5278e48cb87898417760492285
Enrico Testori <hypertesto@gmail.com>
2016-05-21 09:50:17 +0200

    Add wrapper class for documentType

---
+++ app/src/main/java/me/hypertesto/questeasy/model/DocumentType.java
@@ -0,0 +1,28 @@
+package me.hypertesto.questeasy.model;
+
+/**
+ * Created by hypertesto on 21/05/16.
+ */
+public class DocumentType {
+
+	private String code;
+	private String nome;
+
+	public DocumentType() {}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
