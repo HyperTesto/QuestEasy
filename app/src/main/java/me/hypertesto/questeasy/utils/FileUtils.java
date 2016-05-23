f740c2f2357f5c01ee8df7ec57bf7e31f5fdc606
Enrico Testori <hypertesto@gmail.com>
2016-05-23 21:56:11 +0200

    Add file utils class

---
+++ app/src/main/java/me/hypertesto/questeasy/utils/FileUtils.java
@@ -0,0 +1,28 @@
+package me.hypertesto.questeasy.utils;
+
+import android.os.Environment;
+
+/**
+ * Created by hypertesto on 23/05/16.
+ */
+public class FileUtils {
+
+	public boolean isExternalStorageWritable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state)) {
+			return true;
+		}
+		return false;
+	}
+
+	/* Checks if external storage is available to at least read */
+	public boolean isExternalStorageReadable() {
+		String state = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(state) ||
+				Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+			return true;
+		}
+		return false;
+	}
+
+}
