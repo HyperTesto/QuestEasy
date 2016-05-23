6bd92320a9f4c71451e5039cae7924d3ca42917e
Enrico Testori <hypertesto@gmail.com>
2016-05-23 22:05:21 +0200

    Other extra methods

--- app/src/main/java/me/hypertesto/questeasy/utils/FileUtils.java
+++ app/src/main/java/me/hypertesto/questeasy/utils/FileUtils.java
@@ -1,13 +1,21 @@
 package me.hypertesto.questeasy.utils;
 
 import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
 
 /**
  * Created by hypertesto on 23/05/16.
+ * An extra class with some useful method to handle file storage
  */
 public class FileUtils {
 
-	public boolean isExternalStorageWritable() {
+	/**
+	 * Check if external storage is writable
+	 * @return
+	 */
+	public static boolean isExternalStorageWritable() {
 		String state = Environment.getExternalStorageState();
 		if (Environment.MEDIA_MOUNTED.equals(state)) {
 			return true;
@@ -15,14 +23,38 @@ public class FileUtils {
 		return false;
 	}
 
-	/* Checks if external storage is available to at least read */
-	public boolean isExternalStorageReadable() {
+	/**
+	 * Check if external storage is readable
+	 * @return
+	 */
+	public static boolean isExternalStorageReadable() {
 		String state = Environment.getExternalStorageState();
 		if (Environment.MEDIA_MOUNTED.equals(state) ||
 				Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
 			return true;
 		}
 		return false;
+	}
+
+	/**
+	 * Get directory for questura on external storage formatted file, this file it's publicly
+	 * accessible and persist after app unistall.
+	 * @param fileName
+	 * @return
+	 */
+	public static File getFileQuesturaStorageDir(String fileName) {
+
+		File file = null;
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+			file = new File(Environment.getExternalStoragePublicDirectory(
+					Environment.DIRECTORY_DOCUMENTS), fileName);
+		} else {
+			file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
+		}
+		if (!file.mkdirs()) {
+			Log.e("[FILE_DBG]", "Directory not created");
+		}
+		return file;
 	}
 
 }
