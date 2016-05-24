82a2d03329f903624d3110efe0df1d58346dff1f
Enrico Testori <hypertesto@gmail.com>
2016-05-24 18:04:28 +0200

    Add comments

--- app/src/main/java/me/hypertesto/questeasy/utils/AutoCompleteRequest.java
+++ app/src/main/java/me/hypertesto/questeasy/utils/AutoCompleteRequest.java
@@ -7,8 +7,16 @@ import java.util.List;
 import me.hypertesto.questeasy.model.Place;
 
 /**
+ * Interface that define methods for the autocomplete feature to get Places
  * Created by hypertesto on 10/05/16.
  */
 public interface AutoCompleteRequest {
+
+	/**
+	 * Perform the request give the String srt and return a list of Places
+	 * @param context
+	 * @param str
+	 * @return
+	 */
 	List<Place> find(Context context, String str);
 }
