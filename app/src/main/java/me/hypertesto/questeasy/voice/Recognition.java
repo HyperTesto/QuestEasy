a3617d31a509a2f956fa90c8ae8070f9a90b2b45
Enrico Testori <hypertesto@gmail.com>
2016-05-27 16:39:30 +0200

    Personal data parser stub

--- app/src/main/java/me/hypertesto/questeasy/voice/Recognition.java
+++ app/src/main/java/me/hypertesto/questeasy/voice/Recognition.java
@@ -6,9 +6,16 @@ import java.util.regex.Pattern;
 
 import me.hypertesto.questeasy.model.DocumentType;
 import me.hypertesto.questeasy.model.Documento;
+import me.hypertesto.questeasy.model.FamilyHeadGuest;
+import me.hypertesto.questeasy.model.FamilyMemberGuest;
+import me.hypertesto.questeasy.model.GroupHeadGuest;
+import me.hypertesto.questeasy.model.GroupMemberGuest;
 import me.hypertesto.questeasy.model.Guest;
 import me.hypertesto.questeasy.model.Place;
+import me.hypertesto.questeasy.model.SingleGuest;
 import me.hypertesto.questeasy.model.adapters.DocumentTypeAdapter;
+import me.hypertesto.questeasy.utils.DateUtils;
+import me.hypertesto.questeasy.utils.StaticGlobals;
 
 /**
  * Class used to parse results from google speech recognition
@@ -20,18 +27,57 @@ public class Recognition {
 
 	}
 
+
+
 	/**
 	 * Should return a guest with personal info
-	 * Riconosce testo che contiene frasi nella forma:
-	 * - Permanenza %d giorni
-	 * - %d giorni di permanenza
+	 * We pass a type in order to init the right class (guest is abstract), this choice allow us to
+	 * keep activity code more clear and simpler
 	 * @param text
+	 * @param type
 	 * @return
 	 */
-	public Guest parsePersonalInfo (List<String> text){
+	public Guest parsePersonalInfo (String text, String type){
 
+		Guest res = initGuestByType(type);
+		//												(1)				(2)				(3)
+		Pattern personalInfo = Pattern.compile(".*(signor|signora)\\s([a-zA-Z]+)\\s([a-zA-Z]+).*");
+		//												(1)			(2)				(3)			(4)			(5)				(6)
+		Pattern birthInfo = Pattern.compile(".*nato\\s(a|in)\\s([a-zA-Z]+)\\sil\\s(giorno\\s)*([0-9]+)\\s([a-zA-Z0-9]+)\\s([0-9]+).*");
 
-		return null;
+		Pattern citizenship = Pattern.compile(".*cittadinanza\\s([a-zA-Z]+).*");
+
+		//TODO: make sure month is expressed as a number
+		Matcher m1 = personalInfo.matcher(text);
+		Matcher m2 = birthInfo.matcher(text);
+		Matcher m3 = citizenship.matcher(text);
+
+		if (m1.matches()){
+			if (m1.group(1).equals("signor")){
+				res.setSex("M");
+			} else {
+				res.setSex("F");
+			}
+			res.setName(m1.group(2));
+			res.setName(m1.group(3));
+		}
+
+		Place birth = new Place();
+		if (m2.matches()) {
+			birth.setName(m2.group(2));
+			String birthDate = String.format("%s/%s/%s", m2.group(4), m2.group(5), m2.group(6));
+			res.setBirthDate(DateUtils.parse(birthDate));
+		}
+		res.setPlaceOfBirth(birth);
+
+		Place citizen = new Place();
+		if (m3.matches()){
+			citizen.setName(m3.group(1));
+		}
+		res.setCittadinanza(citizen);
+
+		return res;
+
 	}
 
 	/**
@@ -93,5 +139,33 @@ public class Recognition {
 		}
 
 		return -1;
+	}
+
+	/**
+	 * Auxiliary method that correctly init a guest given his type
+	 * @param type
+	 * @return
+	 */
+	private Guest initGuestByType(String type){
+
+		Guest res = null;
+		switch (type){
+			case Guest.type.SINGLE_GUEST:
+				res = new SingleGuest();
+				break;
+			case Guest.type.FAMILY_HEAD:
+				res = new FamilyHeadGuest();
+				break;
+			case Guest.type.GROUP_HEAD:
+				res = new GroupHeadGuest();
+				break;
+			case Guest.type.FAMILY_MEMBER:
+				res = new FamilyMemberGuest();
+				break;
+			case Guest.type.GROUP_MEMBER:
+				res = new GroupMemberGuest();
+				break;
+		}
+		return res;
 	}
 }
