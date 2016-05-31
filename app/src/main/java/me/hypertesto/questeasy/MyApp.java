da071c38255f8f2d5623991642cf148525ed103d
Enrico Testori <hypertesto@gmail.com>
2016-05-31 13:38:28 +0200

    Insert in the report only fields saved by the remote API

--- app/src/main/java/me/hypertesto/questeasy/MyApp.java
+++ app/src/main/java/me/hypertesto/questeasy/MyApp.java
@@ -6,12 +6,14 @@ import android.content.Context;
 import org.acra.ACRA;
 import org.acra.annotation.ReportsCrashes;
 
+import static org.acra.ReportField.*;
+
 /**
  * Created by hypertesto on 31/05/16.
  */
 @ReportsCrashes(
 		formUri = "http://questura.hypertesto.me:8080/api/v1/crashreport",
-		reportType = org.acra.sender.HttpSender.Type.JSON
+		customReportContent = { REPORT_ID, STACK_TRACE, USER_CRASH_DATE }
 )
 public class MyApp extends Application {
 
