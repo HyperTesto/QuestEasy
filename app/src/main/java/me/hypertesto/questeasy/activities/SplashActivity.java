e0bd00159f0a88d2d33974f1155d4b1ea1811987
simoneclr <simone.clclr@gmail.com>
2016-06-01 15:37:43 +0200

    Fix splash activity

--- app/src/main/java/me/hypertesto/questeasy/activities/SplashActivity.java
+++ app/src/main/java/me/hypertesto/questeasy/activities/SplashActivity.java
@@ -10,21 +10,37 @@ import me.hypertesto.questeasy.R;
 public class SplashActivity extends AppCompatActivity {
 
 	private static int SPLASH_TIMEOUT_MILLIS = 3000;
+	Intent homeIntent;
+	Handler handler;
+	Runnable runnable;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
 
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run(){
-				Intent i = new Intent(SplashActivity.this, HomeActivity.class);
-				startActivity(i);
+		homeIntent = new Intent(SplashActivity.this, HomeActivity.class);
 
+		handler = new Handler();
+
+		runnable = new Runnable(){
+			@Override
+			public void run() {
+				startActivity(homeIntent);
 				finish();
 			}
-		}, SPLASH_TIMEOUT_MILLIS);
+		};
+	}
 
+	@Override
+	protected void onResume(){
+		super.onResume();
+		handler.postDelayed(runnable, SPLASH_TIMEOUT_MILLIS);
+	}
+
+	@Override
+	protected void onPause(){
+		super.onPause();
+		handler.removeCallbacks(runnable);
 	}
 }
