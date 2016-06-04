cb7546fc2bda26489661b003c9aff8def45c09ed
Enrico Testori <hypertesto@gmail.com>
2016-06-04 09:14:22 +0200

    :cinema: Add setting to show tutorial on every startup (default to false)

--- app/src/main/java/me/hypertesto/questeasy/MyApp.java
+++ app/src/main/java/me/hypertesto/questeasy/MyApp.java
@@ -2,9 +2,16 @@ package me.hypertesto.questeasy;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import org.acra.ACRA;
 import org.acra.annotation.ReportsCrashes;
+
+import me.hypertesto.questeasy.activities.ActivityGalleryV2;
+import me.hypertesto.questeasy.activities.EditCardActivity;
+import me.hypertesto.questeasy.activities.EditDecActivity;
+import me.hypertesto.questeasy.activities.HomeActivity;
 
 import static org.acra.ReportField.*;
 
@@ -17,9 +24,26 @@ import static org.acra.ReportField.*;
 )
 public class MyApp extends Application {
 
+	public static final String TUTORIAL_ON_STARTUP = "pref_tutorial";
+
 	@Override
 	protected void attachBaseContext(Context base) {
 		super.attachBaseContext(base);
+
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(base);
+
+		/*
+		 * If show tutorial on each startup is true, we overwrite every step value to show them
+		 * NOTE: the very first startup show tutorial anyway.
+		 */
+		if (sharedPref.getBoolean(TUTORIAL_ON_STARTUP, false)){
+			SharedPreferences.Editor editor = sharedPref.edit();
+			editor.putBoolean(HomeActivity.TUTORIAL_FIRST_SHOWN,true);
+			editor.putBoolean(EditDecActivity.TUTORIAL_SECOND_SHOWN,true);
+			editor.putBoolean(ActivityGalleryV2.TUTORIAL_FOURTH_SHOWN, true);
+			editor.putBoolean(EditCardActivity.TUTORIAL_FIFTH_SHOWN, true);
+			editor.apply();
+		}
 
 		// The following line triggers the initialization of ACRA
 		ACRA.init(this);
